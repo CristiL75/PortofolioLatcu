40a029bdfee5e02aafc55381dcd9151eeda4f59e
CristiL75 <68616866+CristiL75@users.noreply.github.com>
2025-02-21 13:40:40 +0200

    Update ProjectCard.jsx

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function ProjectCard({src, link, h3}) {
+function ProjectCard({src, link, h3, p}) {
   return (
      <a href ={link} target = "_blank">
                 <img className = "hover" src = {src} alt = {`${h3} logo`}/>
